25b1739cde2c619256114d0fdc25956daa4e08cc
Andre Schweiger <andre.spiele@yahoo.de>
2022-02-07 11:33:57 +0100

    Switch to self hosted font for consistent styling across devices

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+import '@fontsource/roboto/latin-400.css';
+import '@fontsource/roboto/latin-700.css';
 
 ReactDOM.render(
   <React.StrictMode>
